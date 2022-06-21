0db5af20df3c10678ee2e1a57542364a66ec1516
Vitaliy Yudin <yudin.vitaliy@skyeng.ru>
2022-06-21 18:17:12 +0300

    add create training component

---
+++ src/app/create-training/create-training.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateTrainingComponent } from './component/create-training/create-training.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateTrainingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CreateTrainingModule { }
